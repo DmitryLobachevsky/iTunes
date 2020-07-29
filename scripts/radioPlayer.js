308d3edff748d0528778faf7eebef821dca6118c
DmitryLobachevsky <47788358+DmitryLobachevsky@users.noreply.github.com>
2020-07-29 12:45:53 +0300

    feat: craeted radio volume

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -5,6 +5,7 @@ export const radioPlayerInit = () => {
     const radioNavigation = document.querySelector('.radio-navigation'); 
     const radioItem = document.querySelectorAll('.radio-item'); 
     const radioStop = document.querySelector('.radio-stop'); 
+    const radioVolume = document.querySelector('.radio-volume');
 
     const audio = new Audio();
     audio.type = 'audio/aac';
@@ -56,4 +57,10 @@ export const radioPlayerInit = () => {
         }
         changeIconPlay();
     });
+
+    radioVolume.addEventListener('input', () => {
+        audio.volume = radioVolume.value / 100;
+    });
+
+    
 } 
